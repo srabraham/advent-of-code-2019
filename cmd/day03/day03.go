970992d254fee9864d32fea0fb68bc6547f8fa36
Sean Abraham <seanabraham@gmail.com>
2019-12-02 22:26:51 -0700

    part 1 of day 3

---
+++ cmd/day03/day03.go
@@ -0,0 +1,88 @@
+package main
+
+import (
+	"io/ioutil"
+	"log"
+	"strconv"
+	"strings"
+)
+
+func f(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	b, err := ioutil.ReadFile("cmd/day03/input03-1.txt")
+	f(err)
+	lines := strings.Split(string(b), "\n")
+	w1 := strings.Split(lines[0], ",")
+	w2 := strings.Split(lines[1], ",")
+
+	grid1 := getGrid(w1)
+	grid2 := getGrid(w2)
+
+	log.Print(grid1)
+	log.Print(grid2)
+
+	for x, yMap := range grid1 {
+		for y := range yMap {
+			if grid2[x] != nil && grid2[x][y] {
+				log.Printf("found intersection at %v, %v. === %v", x, y, abs(x) + abs(y))
+			}
+		}
+	}
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
+
+func getGrid(w1 []string) map[int]map[int]bool {
+	grid1 := make(map[int]map[int]bool)
+	var currX, currY int
+	for _, w := range w1 {
+		dir := w[:1]
+		length, err := strconv.Atoi(w[1:])
+		f(err)
+		switch dir {
+		case "U":
+			for y := 0; y < length; y++ {
+				currY++
+				initG(grid1, currX)
+				grid1[currX][currY] = true
+			}
+		case "D":
+			for y := 0; y < length; y++ {
+				currY--
+				initG(grid1, currX)
+				grid1[currX][currY] = true
+			}
+		case "L":
+			for x := 0; x < length; x++ {
+				currX--
+				initG(grid1, currX)
+				grid1[currX][currY] = true
+			}
+		case "R":
+			for x := 0; x < length; x++ {
+				currX++
+				initG(grid1, currX)
+				grid1[currX][currY] = true
+			}
+		default:
+			log.Fatalf("bad %v", dir)
+		}
+	}
+	return grid1
+}
+
+func initG(g map[int]map[int]bool, x int) {
+	if g[x] == nil {
+		g[x] = make(map[int]bool)
+	}
+}
---
+++ internal/intcode/intcode.go
@@ -0,0 +1,70 @@
+package intcode
+
+import (
+"io/ioutil"
+"log"
+"strconv"
+"strings"
+)
+
+func f(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func run(in []int64) []int64 {
+	currPos := 0
+	for {
+		switch in[currPos] {
+		case 1:
+			log.Printf("doing an add: %v, %v, %v, %v", in[currPos], in[currPos+1], in[currPos+2], in[currPos+3])
+			aVal := in[currPos+1]
+			bVal := in[currPos+2]
+			cVal := in[currPos+3]
+			in[cVal] = in[aVal] + in[bVal]
+			currPos += 4
+		case 2:
+			log.Printf("doing an mult: %v, %v, %v, %v", in[currPos], in[currPos+1], in[currPos+2], in[currPos+3])
+			aVal := in[currPos+1]
+			bVal := in[currPos+2]
+			cVal := in[currPos+3]
+			in[cVal] = in[aVal] * in[bVal]
+			currPos += 4
+		case 99:
+			return in
+		default:
+			log.Fatalf("bad opcode %v", in[currPos])
+		}
+	}
+	log.Fatal("shouldn't happen")
+	return nil
+}
+
+func main() {
+	b, err := ioutil.ReadFile("cmd/day02/input02-1a.txt")
+	f(err)
+	nums := strings.Split(string(b), ",")
+	cmds := make([]int64, 0)
+	for _, num := range nums {
+		n, err := strconv.ParseInt(num, 10, 64)
+		f(err)
+		cmds = append(cmds, n)
+	}
+	log.Printf("cmds = %v", cmds)
+	//log.Printf("result = %v", run(cmds))
+
+	for noun := int64(0); noun < 100; noun++ {
+		for verb := int64(0); verb < 100; verb++ {
+			cmdsCopy := make([]int64, len(cmds))
+			copy(cmdsCopy, cmds)
+			cmdsCopy[1] = noun
+			cmdsCopy[2] = verb
+			res := run(cmdsCopy)[0]
+			log.Printf("for noun %v, verb %v got res = %v", noun, verb, res)
+			if res == 19690720 {
+				return
+			}
+		}
+	}
+}
