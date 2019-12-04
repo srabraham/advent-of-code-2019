f9e9fff5040c4f6a188d7b534fd90cf5e014a38b
Sean Abraham <seanabraham@gmail.com>
2019-12-03 22:40:38 -0700

    day04: a bit tidier

--- cmd/day04/day04_test.go
+++ cmd/day04/day04_test.go
@@ -1,3 +1,13 @@
 package main
 
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
 
+func Test_matchesPart1Criteria(t *testing.T) {
+	assert.Equal(t, true, matchesPart1Criteria(11223344))
+	assert.Equal(t, false, matchesPart1Criteria(11223342))
+	assert.Equal(t, true, matchesPart1Criteria(11234))
+
+}
