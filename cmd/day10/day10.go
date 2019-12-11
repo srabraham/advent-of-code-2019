541362e44b084acd527be88f0bd6e943a61c93c6
Sean Abraham <seanabraham@gmail.com>
2019-12-10 19:29:07 -0700

    update day 10

--- cmd/day10/day10.go
+++ cmd/day10/day10.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/srabraham/advent-of-code-2019/internal/seanmath"
 	"io/ioutil"
 	"log"
 	"math"
@@ -14,128 +15,66 @@ func f(err error) {
 	}
 }
 
-func main() {
-	b, err := ioutil.ReadFile("cmd/day10/input10-1.txt")
-	f(err)
-	rows := strings.Split(string(b), "\n")
-	grid := make([][]string, 0)
-	var collisions [][]int
-	for _, r := range rows {
-		var g []string
-		for _, c := range r {
-			g = append(g, string(c))
-		}
-		grid = append(grid, g)
-		collisions = append(collisions, make([]int, len(g)))
-	}
-	log.Print(grid)
-
-	for x := range grid {
-		for y, val := range grid[x] {
-			if val == "#" {
-				collisions[x][y] = countCollisions(grid, x, y)
-			}
-		}
-	}
-	var max int
-	var maxX, maxY int
-	for x, row := range collisions {
-		log.Print(row)
-		for y, v := range row {
-			if v > max {
-				max = v
-				maxX = x
-				maxY = y
-			}
-		}
-	}
-	log.Printf("max = %v, maxX = %v, maxY = %v", max, maxX, maxY)
-
-	// this is part 2. TODO: rename the method
-	getRelAsts(grid, 11,11)
-}
-
-// RelAst is the relative position of an asteroid with respect to some other x,y point
-type RelAst struct {
+type GridPos struct {
 	x int
 	y int
-	distX int
-	distY int
 }
 
-func getRelAsts(grid [][]string, x int, y int) {
-	var ra []RelAst
-	// find all the other asteroids on the board and calculate their positions
-	// relative to the x,y input pair
-	for checkX := range grid {
-		for checkY, checkVal := range grid[checkX] {
-			if checkX == x && checkY == y {
-				continue
-			}
-			if checkVal == "#" {
-				r := RelAst{
-					x: checkX,
-					y: checkY,
-					distX: checkX - x,
-					distY: checkY - y,
-				}
-				ra = append(ra,r)
-				log.Printf("created ra %v", r)
-			}
-		}
-	}
-
-
-	// map of angles to other asteroids on the ray extending at that angle from x,y
-	mappedAngles := make(map[float64][]RelAst)
-
-	for _, r := range ra {
-		angle := math.Atan2(-float64(r.distY), float64(r.distX))
-		mappedAngles[angle] = append(mappedAngles[angle], r)
-	}
-
-	angles := make([]float64, 0)
-	for k := range mappedAngles {
-		angles = append(angles, k)
-	}
-
-	sort.Float64s(angles)
-	for _, s := range mappedAngles {
-		sort.Slice(s, func(i, j int) bool {
-			return math.Sqrt(float64(ra[i].distX)*float64(ra[i].distX)+float64(ra[i].distY)*float64(ra[i].distY)) < math.Sqrt(float64(ra[j].distX)*float64(ra[j].distX)+float64(ra[j].distY)*float64(ra[j].distY))
-		})
-	}
-
-	blast := 0
-	blastsThisPass := 1
-	// each pass is a full 2pi rotation of the blaster ray.
-	// keep doing passes until no asteroids remain.
-	for pass := 0; blastsThisPass > 0; pass++ {
-		blastsThisPass = 0
-		for _, a := range angles {
-			ma, found := mappedAngles[a]
-			if !found {
-				continue
-			}
-			if len(ma) <= pass {
-				continue
-			}
-			blast++
-			blastsThisPass++
-			val := ma[pass]
-			log.Printf("%v entry is %v", blast, val)
-		}
-	}
+type Grid struct {
+	vals map[GridPos]string
+	maxX int
+	maxY int
 }
 
+func (g Grid) ValAt(x, y int) (string, bool) {
+	val, inBounds := g.vals[GridPos{x: x, y: y}]
+	return val, inBounds
+}
 
-func countCollisions(grid [][]string, x int, y int) int {
+func (g Grid) String() string {
+	var s string
+	for y := 0; y < g.maxY; y++ {
+		for x := 0; x < g.maxX; x++ {
+			s += g.vals[GridPos{x: x, y: y}]
+			s += " "
+		}
+		s += "\n"
+	}
+	return s
+}
+
+func ReadGrid(filename string) Grid {
+	b, err := ioutil.ReadFile(filename)
+	f(err)
+	rows := strings.Split(string(b), "\n")
+	grid := Grid{
+		vals: make(map[GridPos]string),
+		maxX: len(rows[0]),
+		maxY: len(rows),
+	}
+	for y, row := range rows {
+		for x, col := range row {
+			grid.vals[GridPos{x: x, y: y}] = string(col)
+			if x > grid.maxX {
+				grid.maxX = x
+			}
+		}
+		if y > grid.maxY {
+			grid.maxY = y
+		}
+	}
+	//log.Printf("grid =\n%v", grid)
+	return grid
+}
+
+func countCollisions(grid Grid, x int, y int) int {
 	collisions := 0
-	for checkX := range grid {
-		for checkY, checkVal := range grid[checkX] {
+	for checkX := 0; checkX < grid.maxX; checkX++ {
+		for checkY := 0; checkY < grid.maxY; checkY++ {
 			if checkX == x && checkY == y {
 				continue
 			}
+			checkVal, _ := grid.ValAt(checkX, checkY)
 			if checkVal == "#" {
 				rise := checkY - y
 				run := checkX - x
@@ -152,11 +91,12 @@ func countCollisions(grid [][]string, x int, y int) int {
 					if xRay == checkX && yRay == checkY {
 						break loop
 					}
-					if xRay < 0 || xRay >= len(grid) || yRay < 0 || yRay >= len(grid[xRay]) {
+					rayVal, inBounds := grid.ValAt(xRay, yRay)
+					if !inBounds {
 						break loop
 					}
-					if grid[xRay][yRay] == "#" {
-						log.Printf("rise %v run %v intersect (%v,%v) <> (%v,%v) <> (%v,%v)", rise, run, x,y,xRay,yRay,checkX,checkY)
+					if rayVal == "#" {
+						//log.Printf("rise %v run %v intersect (%v,%v) <> (%v,%v) <> (%v,%v)", rise, run, x, y, xRay, yRay, checkX, checkY)
 						intersect = true
 						break loop
 					}
@@ -170,6 +110,126 @@ func countCollisions(grid [][]string, x int, y int) int {
 	return collisions
 }
 
+func Part1(filename string) int {
+	g := ReadGrid(filename)
+	collisions := make(map[GridPos]int)
+	for x := 0; x < g.maxX; x++ {
+		for y := 0; y < g.maxY; y++ {
+			val, _ := g.ValAt(x, y)
+			if val == "#" {
+				collisions[GridPos{x: x, y: y}] = countCollisions(g, x, y)
+			}
+		}
+	}
+	var max int
+	var maxX, maxY int
+	for x := 0; x < g.maxX; x++ {
+		//log.Print(row)
+		for y := 0; y < g.maxY; y++ {
+			v := collisions[GridPos{x: x, y: y}]
+			if v > max {
+				max = v
+				maxX = x
+				maxY = y
+			}
+		}
+	}
+	log.Printf("max = %v, maxX = %v, maxY = %v", max, maxX, maxY)
+	return max
+}
+
+// RelAst is the relative position of an asteroid with respect to some other x,y point
+type RelAst struct {
+	pos GridPos
+	distX int
+	distY int
+}
+
+func (ra RelAst) angle() float64 {
+	return math.Atan2(-float64(ra.distX), float64(ra.distY))
+}
+
+func (ra *RelAst) length() float64 {
+	return math.Sqrt(float64(ra.distX*ra.distX+ra.distY*ra.distY))
+}
+
+func Part2(filename string, x, y int) {
+	g := ReadGrid(filename)
+
+	var ra []RelAst
+	// find all the other asteroids on the board and calculate their positions
+	// relative to the x,y input pair
+	for checkX := 0; checkX < g.maxX; checkX++ {
+		for checkY := 0; checkY < g.maxY; checkY++ {
+			if checkX == x && checkY == y {
+				continue
+			}
+			checkVal, _ := g.ValAt(checkX, checkY)
+			if checkVal == "#" {
+				r := RelAst{
+					pos: GridPos{checkX, checkY},
+					distX: checkX - x,
+					distY: checkY - y,
+				}
+				ra = append(ra, r)
+				//log.Printf("created ra %v", r)
+			}
+		}
+	}
+
+	// map of angles to other asteroids on the ray extending at that angle from x,y
+	anglesToAsteroids := make(map[int64][]RelAst)
+
+	for _, r := range ra {
+		angle := int64(math.Round(r.angle()*1_000_000.0))
+		anglesToAsteroids[angle] = append(anglesToAsteroids[angle], r)
+	}
+
+	// all of the known angles
+	angles := make([]int64, 0)
+	for k := range anglesToAsteroids {
+		angles = append(angles, k)
+	}
+	seanmath.Sort(angles)
+	for _, s := range anglesToAsteroids {
+		sort.Slice(s, func(i, j int) bool {
+			log.Printf("compare %v to %v", s[i].length(), s[j].length())
+			return s[i].length() < s[j].length()
+		})
+	}
+
+
+
+	blast := 0
+	blastsThisPass := 1
+	// each pass is a full 2pi rotation of the blaster ray.
+	// keep doing passes until no asteroids remain.
+	g.vals[GridPos{x:x,y:y}] = "S"
+	for pass := 0; blastsThisPass > 0; pass++ {
+		blastsThisPass = 0
+		for _, a := range angles {
+			ma, found := anglesToAsteroids[a]
+			if !found {
+				continue
+			}
+			if len(ma) <= pass {
+				continue
+			}
+			blast++
+			blastsThisPass++
+			val := ma[pass]
+			g.vals[val.pos] = "X"
+			log.Printf("%v entry is %v, grid =\n%v", blast, val, g)
+		}
+	}
+}
+
+func main() {
+	Part1("cmd/day10/input10-1.txt")
+	Part2("cmd/day10/input10-1.txt", 11, 11)
+}
+
+
 // greatest common divisor (GCD) via Euclidean algorithm
 func GCD(a, b int) int {
 	for b != 0 {
@@ -180,6 +240,6 @@ func GCD(a, b int) int {
 	if a < 0 {
 		a = -a
 	}
-	log.Printf("gcd = %v", a)
+	//log.Printf("gcd = %v", a)
 	return a
 }
