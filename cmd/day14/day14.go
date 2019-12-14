1bbb607e71fa3049b11229c2426c17a6e28cea18
Sean Abraham <seanabraham@gmail.com>
2019-12-14 00:20:31 -0700

    day 14 done, except tests. bedtime...

--- cmd/day14/day14.go
+++ cmd/day14/day14.go
@@ -19,6 +19,11 @@ type AmountThing struct {
 	thing  string
 }
 
+type Formulas struct {
+	produceToReq        map[AmountThing][]AmountThing
+	produceStrToProduce map[string]AmountThing
+}
+
 func StringToThing(s string) AmountThing {
 	s1 := strings.TrimSpace(s)
 	splits := strings.Split(s1, " ")
@@ -30,93 +35,96 @@ func StringToThing(s string) AmountThing {
 	return AmountThing{amount: num, thing: splits[1]}
 }
 
-func main() {
-	log.Print("starting...")
-	b, err := ioutil.ReadFile("cmd/day14/input14-0.txt")
-	f(err)
-	rows := strings.Split(string(b), "\n")
-
+func CreateFormulas(rows []string) Formulas {
 	produceStrToProduce := make(map[string]AmountThing)
 	produceToReq := make(map[AmountThing][]AmountThing)
 
 	for _, r := range rows {
 		splits := strings.Split(r, "=>")
 		if len(splits) != 2 {
-			log.Fatal("FDfd")
+			log.Fatalf("bad row in the file: %v", r)
 		}
 		left := strings.TrimSpace(splits[0])
-		right := strings.TrimSpace(splits[1])
-
-		produces := StringToThing(right)
-
+		produces := StringToThing(strings.TrimSpace(splits[1]))
 		var requires []AmountThing
 		for _, s := range strings.Split(left, ",") {
 			requires = append(requires, StringToThing(s))
 		}
-
 		produceToReq[produces] = requires
 		produceStrToProduce[produces.thing] = produces
 	}
+	return Formulas{produceStrToProduce: produceStrToProduce, produceToReq: produceToReq}
+}
 
-	minVal := -1
-
-	var round int64
+// Solve finds the number of ore needed to produce the provided number of fuel.
+func (f Formulas) Solve(requiredFuels int) int {
+	requiredOutputs := make(map[string]int)
+	excess := make(map[string]int)
+	requiredOutputs["FUEL"] = requiredFuels
+	//log.Printf("%v", produceToReq[produceStrToProduce["FUEL"]])
+	alreadyMade := make(map[string]bool)
 	for true {
-		round++
-		have := make(map[string]int)
-		excess := make(map[string]int)
-		have["FUEL"] = 1
-		//log.Printf("%v", produceToReq[produceStrToProduce["FUEL"]])
-		alreadyMade := make(map[string]bool)
-		for true {
-			done := true
-			for k := range have {
-				if have[k] == 0 {
-					continue
-				}
-				if k != "ORE" {
-					done = false
-					produce := produceStrToProduce[k]
-					needToMake := max(produce.amount, have[k])
-					roundsOfInputs := int(math.Ceil(float64(needToMake) / float64(produce.amount)))
-					excess[k] += roundsOfInputs * produce.amount - have[k]
-					reqs := produceToReq[produceStrToProduce[k]]
-					//log.Printf("making %v of %v requires %v mult of %v", needToMake, k, roundsOfInputs, reqs)
-					for _, r := range reqs {
-						have[r.thing] += r.amount * roundsOfInputs
-						for excess[r.thing] > 0 && have[r.thing] > 0 {
-							have[r.thing] -= 1
-							excess[r.thing] -= 1
-						}
+		done := true
+		for k := range requiredOutputs {
+			if requiredOutputs[k] == 0 {
+				continue
+			}
+			if k != "ORE" {
+				done = false
+				produce := f.produceStrToProduce[k]
+				outputUnits := max(produce.amount, requiredOutputs[k])
+				roundsOfInputs := int(math.Ceil(float64(outputUnits) / float64(produce.amount)))
+				excess[k] += roundsOfInputs*produce.amount - requiredOutputs[k]
+				reqs := f.produceToReq[f.produceStrToProduce[k]]
+				//log.Printf("making %v of %v requires %v mult of %v", outputUnits, k, roundsOfInputs, reqs)
+				for _, r := range reqs {
+					requiredOutputs[r.thing] += r.amount * roundsOfInputs
+					for excess[r.thing] > 0 && requiredOutputs[r.thing] > 0 {
+						requiredOutputs[r.thing] -= 1
+						excess[r.thing] -= 1
 					}
-					have[k] = 0
-					alreadyMade[k] = true
-					//log.Printf("after change, have %v", have)
 				}
-				//time.Sleep(500*time.Millisecond)
+				requiredOutputs[k] = 0
+				alreadyMade[k] = true
+				//log.Printf("after change, requiredOutputs %v", requiredOutputs)
 			}
-			// not 169644
-			if done {
-				newV := have["ORE"]
-				if minVal < 0 {
-					minVal = newV
-				} else {
-					minVal = min(newV, minVal)
-				}
-				break
-			}
-
 		}
-		// not 161753 either
-		if round % 10000 == 0 {
-			log.Printf("minVal = %v", minVal)
-
-
-
-
+		if done {
+			break
 		}
 	}
+	return requiredOutputs["ORE"]
+}
 
+func main() {
+	log.Print("starting...")
+	b, err := ioutil.ReadFile("cmd/day14/input14-1.txt")
+	f(err)
+	formulas := CreateFormulas(strings.Split(string(b), "\n"))
+
+	log.Printf("Part 1: minimum ore/fuel is %v", formulas.Solve(1))
+
+	log.Printf("Part 2: starting binary search")
+	availableOre := 1000000000000
+	var round int64
+	minFuel := 0
+	maxFuel := availableOre
+	for minFuel != maxFuel {
+		round++
+		fuel := (maxFuel - minFuel + 1) / 2 + minFuel
+		numOres := formulas.Solve(fuel)
+		if round%10000 == 0 {
+			log.Printf("requiredOutputs %v < %v ?", numOres, availableOre)
+		}
+		if numOres < availableOre {
+			minFuel = fuel
+			log.Printf("can do %v fuel", fuel)
+		} else {
+			maxFuel = fuel - 1
+			log.Printf("can't do ore for %v fuels", fuel)
+		}
+	}
+	log.Printf("Part 2: Done! can make a maximum of %v fuels", maxFuel)
 }
 
 func max(a, b int) int {
@@ -125,7 +133,6 @@ func max(a, b int) int {
 	}
 	return b
 }
-
 
 func min(a, b int) int {
 	if a < b {
